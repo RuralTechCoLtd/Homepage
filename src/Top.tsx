82bb504031f882fc88147d2feb0a0f8e392cb66f
DaigoOgasawara <71718365+DaigoOgasawara@users.noreply.github.com>
2021-08-25 02:13:07 +0900

    [add] ヒーローイメージのコンポーネント化

--- src/Top.tsx
+++ src/Top.tsx
@@ -3,23 +3,13 @@ import About from "./About";
 import Services from "./Services";
 import Team from "./Team";
 import News from "./News";
-import HeroImage1 from "./images/Photos Library.jpg";
 import "./Top.scss";
+import HeroImage from "./HeroImage";
 
 const Top: React.FC = () => {
   return (
     <div className="Top">
-      <div className="heroContainer">
-        <img
-          src={HeroImage1}
-          alt="heroImage"
-          className="heroContainer__image"
-        />
-        <h1 className="heroContainer__corporateName">Rural Tech</h1>
-        <h2 className="heroContainer__message">
-          We will energize Japan from the rural areas.
-        </h2>
-      </div>
+      <HeroImage />
       <About />
       <Services />
       <Team />
