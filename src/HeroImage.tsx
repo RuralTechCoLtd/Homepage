86351b6cb58e1c97336dc4d192079fa23bab9c68
DaigoOgasawara <71718365+DaigoOgasawara@users.noreply.github.com>
2021-10-08 23:29:40 +0900

    [update] HeroImageのスタイリング

--- src/HeroImage.tsx
+++ src/HeroImage.tsx
@@ -4,12 +4,12 @@ import "./HeroImage.scss";
 
 const HeroImage: React.FC = () => {
   return (
-    <div className="heroContainer">
-      <img src={HeroImage1} alt="heroImage" className="heroContainer__image" />
-      <h1 className="heroContainer__corporateName">Rural Tech</h1>
-      <h2 className="heroContainer__message">
+    <div className="HeroContainer">
+      {/* <img src={HeroImage1} alt="heroImage" className="heroContainer__image" /> */}
+      <h2 className="HeroContainer__message">
         We will energize Japan from the rural areas.
       </h2>
+      <h1 className="HeroContainer__corporateName">Rural Tech</h1>
     </div>
   );
 };
