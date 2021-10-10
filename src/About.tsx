b31917873455a372e7afffaa59955f98b02c60e9
DaigoOgasawara <71718365+DaigoOgasawara@users.noreply.github.com>
2021-10-10 10:24:48 +0900

    [update] Aboutページのスタイリングの前準備

--- src/About.tsx
+++ src/About.tsx
@@ -5,32 +5,34 @@ import "./About.scss";
 
 const About: React.FC = () => {
   return (
-    <React.Fragment>
-      <p>introduction</p>
-      <p>
-        オンラインサロンや
-        <br />
-        IT勉強会で
-        <br />
-        地域貢献を
-      </p>
-      <h1>Rural Tech</h1>
-      <p>
-        ルーラルテックはITで地方から日本を元気づけます！
-        先進のテクノロジーを地方から。
-        <br />
-        独自のアイディアによる地域特化のアプリケーションや
-        <br />
-        お客さまのニーズに寄り添う業務アプリケーションも
-        <br />
-        作成します。
-      </p>
-      <Link to="/About/AboutDetails">
-        <a>
-          About us <DoubleArrowIcon />
-        </a>
-      </Link>
-    </React.Fragment>
+    <div className="About">
+      <div className="About__container">
+        <p>introduction</p>
+        <p>
+          オンラインサロンや
+          <br />
+          IT勉強会で
+          <br />
+          地域貢献を
+        </p>
+        <h1>Rural Tech</h1>
+        <p>
+          ルーラルテックはITで地方から日本を元気づけます！
+          先進のテクノロジーを地方から。
+          <br />
+          独自のアイディアによる地域特化のアプリケーションや
+          <br />
+          お客さまのニーズに寄り添う業務アプリケーションも
+          <br />
+          作成します。
+        </p>
+        <Link to="/About/AboutDetails">
+          <a>
+            About us <DoubleArrowIcon />
+          </a>
+        </Link>
+      </div>
+    </div>
   );
 };
 
