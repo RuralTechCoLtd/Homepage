da004cd0552d6184d4057467468668afab3ca0c0
DaigoOgasawara <71718365+DaigoOgasawara@users.noreply.github.com>
2021-11-14 11:45:04 +0900

    [update] News,NewsArchive,NewsDetailsのスタイリング #22

--- src/News.tsx
+++ src/News.tsx
@@ -11,100 +11,111 @@ type Props = {
   date: string;
   title: string;
   article: string;
+  to: string;
 };
 const NewsArticle: React.FC<Props> = (props: Props) => {
   return (
-    <React.Fragment>
-      <img src={props.image} width="470" height="320"></img>
-      <h3>{props.date}</h3>
-      <h2>{props.title}</h2>
-      <p>{props.article}</p>
-    </React.Fragment>
+    <Link to={props.to} className="News__item">
+      <img src={props.image} className="News__item__img"></img>
+      <h3 className="News__item__date">{props.date}</h3>
+      <h2 className="News__item__title">{props.title}</h2>
+      <p className="News__item__para">{props.article}</p>
+    </Link>
   );
 };
 const News: React.FC = () => {
   return (
     <div className="News">
-      <h2>News</h2>
-      <Link to="/News/Archive">Archive &gt;&gt;</Link>
-      <NewsArticle
-        image={SampleImage1ForNews}
-        date="Jan 27"
-        title="ホームページ制作セミナーを
+      <h2 className="News__title">News</h2>
+      <Link to="/News/Archive" className="News__toArchive">
+        Archive &gt;&gt;
+      </Link>
+      <div className="News__container">
+        <NewsArticle
+          to="/News/Details"
+          image={SampleImage1ForNews}
+          date="Jan 27"
+          title="ホームページ制作セミナーを
         開催しました"
-        article="総勢30名の方にご参加いただき、
+          article="総勢30名の方にご参加いただき、
         充実したセミナーだったと受講者の皆さんからあコメントをいただきました。
         もっと難易度の高いプログラミングをしてみたいなどのご意見や、
         セミナーの改善点などのご提案もいただき大変励みになりました。
         
         次のレッスンははGitの使い方やホームページの公開を題材にセミナーを開催させていただきます。
         ぜひご参加ください。"
-      />
-      <NewsArticle
-        image={SampleImage2ForNews}
-        date="Jan 27"
-        title="ホームページ制作セミナーを
+        />
+        <NewsArticle
+          to="#"
+          image={SampleImage2ForNews}
+          date="Jan 27"
+          title="ホームページ制作セミナーを
         開催しました"
-        article="総勢30名の方にご参加いただき、
+          article="総勢30名の方にご参加いただき、
         充実したセミナーだったと受講者の皆さんからあコメントをいただきました。
         もっと難易度の高いプログラミングをしてみたいなどのご意見や、
         セミナーの改善点などのご提案もいただき大変励みになりました。
         
         次のレッスンははGitの使い方やホームページの公開を題材にセミナーを開催させていただきます。
         ぜひご参加ください。"
-      />
-      <NewsArticle
-        image={SampleImage3ForNews}
-        date="Jan 27"
-        title="ホームページ制作セミナーを
+        />
+        <NewsArticle
+          to="#"
+          image={SampleImage3ForNews}
+          date="Jan 27"
+          title="ホームページ制作セミナーを
         開催しました"
-        article="総勢30名の方にご参加いただき、
+          article="総勢30名の方にご参加いただき、
         充実したセミナーだったと受講者の皆さんからあコメントをいただきました。
         もっと難易度の高いプログラミングをしてみたいなどのご意見や、
         セミナーの改善点などのご提案もいただき大変励みになりました。
         
         次のレッスンははGitの使い方やホームページの公開を題材にセミナーを開催させていただきます。
         ぜひご参加ください。"
-      />
-      <NewsArticle
-        image={SampleImage1ForNews}
-        date="Jan 27"
-        title="ホームページ制作セミナーを
+        />
+        <NewsArticle
+          to="#"
+          image={SampleImage1ForNews}
+          date="Jan 27"
+          title="ホームページ制作セミナーを
         開催しました"
-        article="総勢30名の方にご参加いただき、
+          article="総勢30名の方にご参加いただき、
         充実したセミナーだったと受講者の皆さんからあコメントをいただきました。
         もっと難易度の高いプログラミングをしてみたいなどのご意見や、
         セミナーの改善点などのご提案もいただき大変励みになりました。
         
         次のレッスンははGitの使い方やホームページの公開を題材にセミナーを開催させていただきます。
         ぜひご参加ください。"
-      />
-      <NewsArticle
-        image={SampleImage2ForNews}
-        date="Jan 27"
-        title="ホームページ制作セミナーを
+        />
+        <NewsArticle
+          to="#"
+          image={SampleImage2ForNews}
+          date="Jan 27"
+          title="ホームページ制作セミナーを
         開催しました"
-        article="総勢30名の方にご参加いただき、
+          article="総勢30名の方にご参加いただき、
         充実したセミナーだったと受講者の皆さんからあコメントをいただきました。
         もっと難易度の高いプログラミングをしてみたいなどのご意見や、
         セミナーの改善点などのご提案もいただき大変励みになりました。
         
         次のレッスンははGitの使い方やホームページの公開を題材にセミナーを開催させていただきます。
         ぜひご参加ください。"
-      />
-      <NewsArticle
-        image={SampleImage3ForNews}
-        date="Jan 27"
-        title="ホームページ制作セミナーを
+        />
+        <NewsArticle
+          to="#"
+          image={SampleImage3ForNews}
+          date="Jan 27"
+          title="ホームページ制作セミナーを
         開催しました"
-        article="総勢30名の方にご参加いただき、
+          article="総勢30名の方にご参加いただき、
         充実したセミナーだったと受講者の皆さんからあコメントをいただきました。
         もっと難易度の高いプログラミングをしてみたいなどのご意見や、
         セミナーの改善点などのご提案もいただき大変励みになりました。
         
         次のレッスンははGitの使い方やホームページの公開を題材にセミナーを開催させていただきます。
         ぜひご参加ください。"
-      />
+        />
+      </div>
     </div>
   );
 };
