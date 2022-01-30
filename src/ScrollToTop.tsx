69b012c12fd40f92035da67865be21fb453ac40e
DaigoOgasawara <71718365+DaigoOgasawara@users.noreply.github.com>
2022-01-30 12:35:00 +0900

    [fix] any型によるWarningの修正

--- src/ScrollToTop.tsx
+++ src/ScrollToTop.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { useEffect } from "react";
 import { useLocation, withRouter } from "react-router-dom";
 
-const ScrollToTop: React.FC = (props: any) => {
+const ScrollToTop: React.FC = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
-  return props.children;
+  return null;
 };
 
 export default withRouter(ScrollToTop);
