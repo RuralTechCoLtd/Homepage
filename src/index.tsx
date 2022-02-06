925954c2acce507b11cfac99684456d7881d7f98
Ryo-Engineer <r.yamanouchi.business@gmail.com>
2022-02-06 18:18:01 +0900

    [fix] 想定外の挙動になったため、Material-UIの`CssBaseline`を削除

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { CssBaseline } from "@mui/material";
-
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -10,7 +8,6 @@ import "./index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
