9712454cf948c7f98e8790f2fbd9ce9e4a9b3f94
DaigoOgasawara <71718365+DaigoOgasawara@users.noreply.github.com>
2022-03-15 12:34:03 +0900

    [update] 交差監視APIが機能するかの検証

--- src/About.tsx
+++ src/About.tsx
@@ -2,47 +2,56 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+import { useInView } from "react-intersection-observer";
+
 import { DoubleArrow } from "@mui/icons-material";
 
 import "./About.scss";
 
 const About: React.FC = () => {
+  const { ref, inView } = useInView({
+    rootMargin: "-350px", // ref要素が現れてから50px過ぎたら
+    triggerOnce: true, // 最初の一度だけ実行
+  });
+
   return (
-    <div className="About">
-      <div className="About__container">
-        <p className="About__introduction">introduction</p>
-        <p className="About__copy">
-          オンラインサロンや
-          <br />
-          IT勉強会で
-          <br />
-          地域貢献を
-        </p>
-        <div className="About__childContainer">
-          <h2 className="About__title">Rural Tech</h2>
-          <p className="About__article">
-            ルーラルテックはITで地方から日本を元気づけます！
-            先進のテクノロジーを地方から。
+    <div ref={ref} className="About">
+      {inView && (
+        <div className="About__container">
+          <p className="About__introduction">introduction</p>
+          <p className="About__copy">
+            オンラインサロンや
             <br />
-            独自のアイディアによる地域特化のアプリケーションや
+            IT勉強会で
             <br />
-            お客さまのニーズに寄り添う業務アプリケーションも 作成します。
+            地域貢献を
           </p>
-          <Link className="About__link" to="/AboutDetails">
-            About Us
-            <DoubleArrow
-              sx={{
-                marginLeft: "15px",
-                verticalAlign: "middle",
-                border: "solid 1px",
-                borderRadius: "50%",
-                padding: "15px",
-                fontSize: 50,
-              }}
-            />
-          </Link>
+          <div className="About__childContainer">
+            <h2 className="About__title">Rural Tech</h2>
+            <p className="About__article">
+              ルーラルテックはITで地方から日本を元気づけます！
+              先進のテクノロジーを地方から。
+              <br />
+              独自のアイディアによる地域特化のアプリケーションや
+              <br />
+              お客さまのニーズに寄り添う業務アプリケーションも 作成します。
+            </p>
+            <Link className="About__link" to="/AboutDetails">
+              About Us
+              <DoubleArrow
+                sx={{
+                  marginLeft: "15px",
+                  verticalAlign: "middle",
+                  border: "solid 1px",
+                  borderRadius: "50%",
+                  padding: "15px",
+                  fontSize: 50,
+                }}
+              />
+            </Link>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
