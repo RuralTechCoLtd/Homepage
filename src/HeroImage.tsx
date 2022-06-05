26d0b8f5dce6fb0a2062e2d2a983db2bec638f60
DaigoOgasawara <71718365+DaigoOgasawara@users.noreply.github.com>
2022-06-05 13:08:31 +0900

    [add] ヒーローイメージの変化処理　実装その１

--- src/HeroImage.tsx
+++ src/HeroImage.tsx
@@ -2,9 +2,29 @@ import React from "react";
 
 import "./HeroImage.scss";
 
+import WomanTakingPicture from "./images/designMaterial/background/WomanTakingPicture.webp";
+import Man1 from "./images/designMaterial/background/ManBesideTheRiver1.webp";
+import Looking from "./images/designMaterial/background/LookingAtWaterfront.webp";
+
+const images = [WomanTakingPicture, Man1, Looking];
+
+const coordinate = [];
+
 const HeroImage: React.FC = () => {
+  const [value, setValue] = React.useState(0);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((v) => (v === 2 ? 0 : v + 1));
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
   return (
-    <div className="HeroContainer" id="#">
+    <div
+      className="HeroContainer"
+      id="#"
+      style={{ background: "url(" + images[value] + ")" }}
+    >
       <h2 className="HeroContainer__message">
         We will energize Japan from the rural areas.
       </h2>
